9bed8cfd39f2bfab2b202de390ffccaa75691601
Ariel <Ariel.Salem1989@gmail.com>
2017-05-04 07:01:47 -0700

    Added Bootstrap and ReactBootstrap dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import {Provider} from 'react-redux';
-import logo from './logo.svg';
+import Home from './Home/Home';
+import Kitchen from './Kitchen/Kitchen';
+import Bank from './Bank/Bank';
 import {
   HashRouter as Router,
-  Route,
-  Link
+  Route
 } from 'react-router-dom';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Adultify</h2>
+      <Router>
+        <div className="container-fluid">
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/Kitchen" component={Kitchen}></Route>
+          <Route exact path="/Bank" component={Bank}></Route>
         </div>
-        <p className="App-intro">
-          Plantr is an online gardening and outdoor decor service, enabling people to create and plan their seasonal gardens based on regional variations and personal needs. Plantr was started by a group of passionate urban farmers looking to maximize their lots while incorporating the tech they use to help connect with their garden. It is through the passion of our users that Plantr is able to tell the story of who we are and how we connect with one another. Click <a href='#'>here</a> to get started.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
